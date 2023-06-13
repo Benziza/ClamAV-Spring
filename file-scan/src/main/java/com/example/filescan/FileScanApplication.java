0a6df45cbf60e9afd3f689f8086af65fe1e5be91
Benziza <benizizamohamed@gmail.com>
2023-06-13 20:34:24 +0000

    Prepare Env

---
+++ file-scan/src/main/java/com/example/filescan/FileScanApplication.java
@@ -0,0 +1,13 @@
+package com.example.filescan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileScanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileScanApplication.class, args);
+    }
+
+}
